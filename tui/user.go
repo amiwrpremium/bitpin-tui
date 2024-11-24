35eaa1fef36089421ef1c7d6895bfcb3397be865
AMiWR <amiwrpremium@gmail.com>
2024-11-24 16:06:03 +0330

    feat(tui): enhance balance management features
    - Introduced a `createBalanceTable` function:
      - Dynamically creates a table for displaying balances with filtering options.
      - Utilizes formatted and styled cells for a better visual representation.
    
    - Enhanced the `balances` function:
      - Added a checkbox to exclude zero balances.
      - Included an input field for specifying a refresh interval.
      - Added support for filtering balances by specific assets using a comma-separated list.
      - Supports live updates when a refresh interval is provided.
    
    - Improved user experience:
      - Real-time updates for live balance view with a specified interval.
      - Excluded zero balances dynamically based on user input.
      - Allowed flexible balance filtering using asset names.
    
    - Utilized reusable and modular functions for better code maintainability.

--- tui/user.go
+++ tui/user.go
@@ -2,69 +2,80 @@ package tui
 
 import (
 	bpclient "bitpin-tui/bitpin_client"
+	"bitpin-tui/utils"
 	"fmt"
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
+	"strconv"
+	"strings"
 	"time"
 )
 
+func createBalanceTable(balances []bpclient.Balance, excludeZero bool) *tview.Table {
+	table := tview.NewTable().
+		SetBorders(true)
+
+	table.SetCell(0, 0, tview.NewTableCell("Asset").SetTextColor(tcell.ColorYellow))
+	table.SetCell(0, 1, tview.NewTableCell("Balance").SetTextColor(tcell.ColorYellow))
+	table.SetCell(0, 2, tview.NewTableCell("Frozen").SetTextColor(tcell.ColorYellow))
+
+	iter := 0
+
+	for _, balance := range balances {
+		floatBalance, err := strconv.ParseFloat(balance.Balance, 64)
+		if err != nil {
+			errorModal(nil, fmt.Sprintf("Failed to parse balance: %v", err), nil)
+			return nil
+		}
+		if excludeZero && floatBalance == 0 {
+			continue
+		}
+
+		table.SetCell(iter+1, 0, tview.NewTableCell(balance.Asset).SetTextColor(tcell.ColorGreen))
+		table.SetCell(iter+1, 1, tview.NewTableCell(utils.FormatWithCommas(balance.Balance)))
+		table.SetCell(iter+1, 2, tview.NewTableCell(utils.FormatWithCommas(balance.Frozen)))
+
+		iter++
+	}
+
+	return table
+}
+
+func getBalanceTable(app *tview.Application, excludeZero bool, assets []string) *tview.Table {
+	blnc, err := client.GetBalances(bpclient.GetBalancesParams{Limit: 1000, Assets: assets})
+	if err != nil {
+		errorModal(app, fmt.Sprintf("Failed to get balances: %v", err), balances)
+		return nil
+	}
+
+	return createBalanceTable(blnc, excludeZero)
+}
+
 func balances(app *tview.Application) {
 	form := tview.NewForm()
-	form.AddCheckbox("Non Zero Balances", false, nil).
-		AddCheckbox("Live", false, nil)
+	form.AddCheckbox("Exclude Zero Balances", true, nil).
+		AddInputField("Interval", "-1", 0, nil, nil).
+		AddInputField("Assets", "", 0, nil, nil).
+		AddButton("Submit", func() {
+			excludeNonZero := form.GetFormItemByLabel("Exclude Zero Balances").(*tview.Checkbox).IsChecked()
+			interval, _ := strconv.Atoi(form.GetFormItemByLabel("Interval").(*tview.InputField).GetText())
+			assets := form.GetFormItemByLabel("Assets").(*tview.InputField).GetText()
 
-	form.AddButton("Submit", func() {
-		live := form.GetFormItemByLabel("Live").(*tview.Checkbox).IsChecked()
-		//nonZero := form.GetFormItemByLabel("Non Zero Balances").(*tview.Checkbox).IsChecked()
+			if interval > 0 {
+				for {
+					app.SetRoot(tview.NewFlex(), true)
+					table := getBalanceTable(app, excludeNonZero, strings.Split(assets, ","))
+					app.SetRoot(table, true)
+					time.Sleep(time.Duration(interval) * time.Second)
 
-		if live {
-			table := tview.NewTable().
-				SetBorders(true)
-
-			table.SetCell(0, 0, tview.NewTableCell("Asset").SetTextColor(tcell.ColorYellow))
-			table.SetCell(0, 1, tview.NewTableCell("Balance").SetTextColor(tcell.ColorYellow))
-			table.SetCell(0, 2, tview.NewTableCell("Frozen").SetTextColor(tcell.ColorYellow))
-
-			for {
-				blnc, err := client.GetBalances(bpclient.GetBalancesParams{Limit: 1000})
-				if err != nil {
-					errorModal(app, fmt.Sprintf("Failed to get balances: %v", err), balances)
-					return
+					app.Draw()
 				}
 
-				for i, balance := range blnc {
-					table.SetCell(i+1, 0, tview.NewTableCell(balance.Asset).SetTextColor(tcell.ColorGreen))
-					table.SetCell(i+1, 1, tview.NewTableCell(balance.Balance))
-					table.SetCell(i+1, 2, tview.NewTableCell(balance.Frozen))
-				}
-
+			} else {
+				table := getBalanceTable(app, excludeNonZero, strings.Split(assets, ","))
 				app.SetRoot(table, true)
-				time.Sleep(5 * time.Second)
 			}
-
-		} else {
-			blnc, err := client.GetBalances(bpclient.GetBalancesParams{Limit: 1000})
-			if err != nil {
-				errorModal(app, fmt.Sprintf("Faileds to get balances: %v", err), balances)
-				return
-			}
-
-			table := tview.NewTable().
-				SetBorders(true)
-
-			table.SetCell(0, 0, tview.NewTableCell("Asset").SetTextColor(tcell.ColorYellow))
-			table.SetCell(0, 1, tview.NewTableCell("Balance").SetTextColor(tcell.ColorYellow))
-			table.SetCell(0, 2, tview.NewTableCell("Frozen").SetTextColor(tcell.ColorYellow))
-
-			for i, balance := range blnc {
-				table.SetCell(i+1, 0, tview.NewTableCell(balance.Asset).SetTextColor(tcell.ColorGreen))
-				table.SetCell(i+1, 1, tview.NewTableCell(balance.Balance))
-				table.SetCell(i+1, 2, tview.NewTableCell(balance.Frozen))
-			}
-
-			app.SetRoot(table, true)
-		}
-	})
+		})
 
 	form.
 		AddButton("Back", func() {
