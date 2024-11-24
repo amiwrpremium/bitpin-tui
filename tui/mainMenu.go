f69285400324d655083d8bc9e3f057ea61dce3e9
AMiWR <amiwrpremium@gmail.com>
2024-11-24 06:36:44 +0330

    feat(tui): add main menu for navigating TUI features
    - Implemented `mainMenu` as the central navigation hub for the application:
      - Displays options dynamically based on user authentication status
      - Includes the following features for authenticated users:
        - `Get Balances`: View account balances
        - `Get Open Orders`: View active orders
        - `Create Order`: Place a new market or limit order
        - `Cancel Order`: Cancel a specific order by ID
        - `Pussy Out`: Batch cancel all open orders
        - `Refresh`: Refresh session data by re-authenticating
        - `Logout`: Log out and clear session data
      - Includes options for all users:
        - `Get Order Book`: View real-time order book data
        - `Get Ticker`: View current market tickers
        - `Get Recent Trades`: View recent trade history
        - `Reset`: Reset the database to its default state
        - `Quit`: Exit the application
    - Dynamically updates the menu title to include the logged-in user's full name
    - Utilized `tview` forms for a clean and interactive menu interface
    - Ensured seamless navigation between features with well-structured callbacks

---
+++ tui/mainMenu.go
@@ -0,0 +1,59 @@
+package tui
+
+import (
+	"github.com/rivo/tview"
+)
+
+func mainMenu(app *tview.Application) {
+	form := tview.NewForm()
+
+	form.AddButton("Get Order Book", func() {
+		orderBook(app)
+	}).AddButton("Get Ticker", func() {
+		tickers(app)
+	}).AddButton("Get Recent Trades", func() {
+		recentTrades(app)
+	})
+
+	if user != nil {
+		form.AddButton("Get Balances", func() {
+			balances(app)
+		}).AddButton("Get Open Orders", func() {
+			openOrders(app)
+		}).AddButton("Create Order", func() {
+			createOrder(app)
+		}).AddButton("Cancel Order", func() {
+			cancelOrder(app)
+		}).AddButton("Pussy Out", func() {
+			pussyOut(app)
+		}).AddButton("Refresh", func() {
+			firstLogin()
+		}).AddButton("Logout", func() {
+			logout(app)
+		})
+	} else {
+		form.AddButton("Login", func() {
+			login(app)
+		})
+	}
+
+	form.AddButton("Reset", func() {
+		reset(app)
+	}).AddButton("Quit", func() {
+		app.Stop()
+	})
+
+	title := "Bitpin TUI"
+
+	if user != nil {
+		title += " - " + user.Fullname
+	}
+
+	form.
+		SetBorder(true).
+		SetTitle(title).
+		SetTitleAlign(tview.AlignLeft).
+		SetRect(0, 0, 30, 10)
+
+	app.SetRoot(form, true)
+}
